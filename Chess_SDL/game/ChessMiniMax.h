02983b27e4fbb12bbb3f9bcfb4bbe319c91570aa
Dolev <Dolev@DESKTOP-CHDHQ9K>
2017-09-28 19:06:29 +0300

    Finished documentation of the folder "game". WHAT ABOUT GRAPHICS??????

--- Chess_SDL/game/ChessMiniMax.h
+++ Chess_SDL/game/ChessMiniMax.h
@@ -15,13 +15,38 @@
 #include <string.h>
 #include <limits.h>
 #include "ChessGame.h"
-
+/**
+ * Name: chessMinimaxSuggestMove
+ * Description:
+ * That is a shell function to chessRecursiveMiniMax.
+ * eventually, returns a Locations array which contains the source of the piece that it decided to move
+ * and also the destination - where the piece will be moved.
+ */
 Location* chessMinimaxSuggestMove(ChessGame* game, unsigned int maxDepth, int* type);
-
+/**
+ * Name: chessRecursiveMiniMax
+ * Description:
+ * computing what is the best move the computer can take against the user due to a MiniMax
+ * algorithm which uses an scoring function that rates the gameBoard in different conditions.
+ * It also uses alpha-beta pruning in order to reduce computations and to optimize the program.
+ * That's a recursive function because it moves forward with steps in the game.
+ * returns the score that was computed.
+ */
 int chessRecursiveMiniMax(ChessGame* game,unsigned int maxDepth,bool maxFlag, int alpha, int beta);
-
+/**
+ * Name: scoringFunction
+ * Description:
+ * This is the function that computes the scoring function of the current condition
+ * of the game gameboard.
+ * returns the score that was computed.
+ */
 int scoringFunction(ChessGame* game);
-
+/**
+ * Name: pieceScoreFunction
+ * Description:
+ * returns the score of a specific piece due to the current player:
+ * positive for computer turn, negative for user turn.
+ */
 int pieceScoreFunction(Piece* piece,Color positiveColor);
 
 #endif /* CHESSMINIMAX_H_ */
--- Chess_SDL/game/MainAux.h
+++ Chess_SDL/game/MainAux.h
@@ -8,9 +8,19 @@
 #ifndef MAINAUX_H_
 #define MAINAUX_H_
 #include "ChessMiniMax.h"
-
+/**
+ * Name: chessPlayCom
+ * Description:
+ * A function that handles the computer's turn and play the game
+ *  as it told her by the AI (MiniMax procedure).
+ */
 GAME_MESSAGE chessPlayCom(ChessGame* game,bool consoleMode);
-
+/**
+ * Name: printComMove
+ * Description:
+ * prints the move that the computer took in the following format:
+ * "Computer: move <PieceName> at <PieceSource> to <PieceDestination>\n"
+ */
 void printComMove(ChessGame* game,Location* move);
 
 #endif /* MAINAUX_H_ */
